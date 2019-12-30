52cb6371b2760778cd150a1767cc5c00dac08b4b
Ahmad Zafrullah <23Pstars@gmail.com>
2019-12-30 10:56:36 +0800

    updates

--- script.js
+++ script.js
@@ -1,38 +1,75 @@
+const refresh = 4000;
+
 window.onload = function () {
 
     this.setTimeout(function () {
 
-        const id = window.location.href.replace('https://www.youtube.com/watch?v=', '');
-        const title = document.getElementsByTagName('h1')[0].innerText;
-        const views = document.getElementsByClassName('view-count style-scope yt-view-count-renderer')[0].innerText.replace(/[^0-9]/g, '');
-        const duration = document.getElementsByClassName('ytp-time-duration')[0].innerText;
-        const date = document.getElementsByClassName('style-scope ytd-video-primary-info-renderer')[14].innerText;
-        const likes = document.getElementsByClassName('style-scope ytd-toggle-button-renderer style-text')[1].getAttribute('aria-label').replace(/[^0-9]/g, '');
-        const dislikes = document.getElementsByClassName('style-scope ytd-toggle-button-renderer style-text')[3].getAttribute('aria-label').replace(/[^0-9]/g, '');
-        const comments = document.getElementsByClassName('count-text style-scope ytd-comments-header-renderer')[0].innerText.replace(/[^0-9]/g, '');
+        const eTitle = document.getElementsByTagName('h1')[0];
+        const eViews = document.getElementsByClassName('view-count style-scope yt-view-count-renderer')[0];
+        const eDuration = document.getElementsByClassName('ytp-time-duration')[0];
+        const eDate = document.getElementsByClassName('style-scope ytd-video-primary-info-renderer')[14];
+        const eLikes = document.getElementsByClassName('style-scope ytd-toggle-button-renderer style-text')[1];
+        const eDislikes = document.getElementsByClassName('style-scope ytd-toggle-button-renderer style-text')[3];
+        const eComments = document.getElementsByClassName('count-text style-scope ytd-comments-header-renderer')[0];
 
-        // console.log('Title:', title);
-        // console.log('Views:', views);
-        // console.log('Duration:', duration);
-        // console.log('Date:', date);
-        // console.log('Likes:', likes);
-        // console.log('Dislikes:', dislikes);
-        // console.log('Comments:', comments);
+        const eNextVideo = document.getElementsByClassName('yt-simple-endpoint style-scope ytd-compact-video-renderer');
+        const eFirstNextVideo = eNextVideo[0];
 
-        const Http = new XMLHttpRequest();
-        const url = 'https://zaf.web.id/labs/youtube-metadata/store.php';
+        if (eNextVideo == null)
+            location.reload();
 
-        Http.open('POST', url);
-        Http.send(JSON.stringify([id, title, duration, date, views, likes, dislikes, comments]));
-
-        Http.onreadystatechange = function () {
-            if (Http.status == 200) {
-                // console.log(Http.responseText);
-                window.location = document.getElementsByClassName('yt-simple-endpoint style-scope ytd-compact-video-renderer')[0].href;
-            }
+        else if (eTitle == null || eViews == null || eDuration == null ||
+            eDate == null || eLikes == null || eDislikes == null || eComments == null) {
+            // const idNextVideo = Math.floor(Math.random() * 6);
+            const idNextVideo = 0;
+            window.location = eNextVideo[idNextVideo].href;
         }
 
-    }, 7000);
+        else {
+
+            if (eLikes.getAttribute('aria-label') == null || eLikes.getAttribute('aria-label') == null) {
+
+                const idNextVideo = Math.floor(Math.random() * 3);
+                // const idNextVideo = 0;
+                window.location = eNextVideo[idNextVideo].href;
+
+            } else {
+
+                const id = window.location.href.replace('https://www.youtube.com/watch?v=', '');
+                const title = eTitle.innerText;
+                const views = eViews.innerText.replace(/[^0-9]/g, '');
+                const duration = eDuration.innerText;
+                const date = eDate.innerText;
+                const likes = eLikes.getAttribute('aria-label').replace(/[^0-9]/g, '');
+                const dislikes = eLikes.getAttribute('aria-label').replace(/[^0-9]/g, '');
+                const comments = eComments.innerText.replace(/[^0-9]/g, '');
+
+                // console.log('Title:', title);
+                // console.log('Views:', views);
+                // console.log('Duration:', duration);
+                // console.log('Date:', date);
+                // console.log('Likes:', likes);
+                // console.log('Dislikes:', dislikes);
+                // console.log('Comments:', comments);
+
+                const Http = new XMLHttpRequest();
+                const url = 'https://zaf.web.id/labs/youtube-metadata/store.php';
+
+                Http.open('POST', url);
+                Http.send(JSON.stringify([id, title, duration, date, views, likes, dislikes, comments]));
+
+                Http.onreadystatechange = function () {
+                    if (Http.status == 200) {
+                        // console.log(Http.responseText);
+                        window.location = eFirstNextVideo.href;
+                    }
+                }
+
+            }
+
+        }
+
+    }, refresh);
 
 }
 
