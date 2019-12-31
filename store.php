d3c392f60413b3048173bc31a30945d48c812867
23Pstars <23Pstars@gmail.com>
2020-01-01 07:58:23 +0800

    insert to DB instead of file

--- store.php
+++ store.php
@@ -1,11 +1,19 @@
-<?php 
+<?php
+
+$host = 'host';
+$user = 'user';
+$pass = 'pass';
+$db = 'db';
+
+$pdo = new PDO('mysql:host=' . $host . ';dbname=' . $db, $user, $pass);
 
 header('Access-Control-Allow-Origin: *');
-$data = json_decode(file_get_contents( "php://input" ));
+$data = json_decode(file_get_contents("php://input"));
 
 array_splice($data, 2, 0, array(date('Y-m-d H:i:s')));
 
-$out = fopen('data.csv', 'a+');
-fputcsv($out, $data);
+// $out = fopen('data.csv', 'a+');
+// fputcsv($out, $data);
 
-?>
+$insert = $pdo->prepare('INSERT INTO `log` VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
+$insert->execute($data);
